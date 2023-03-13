f96691a4ce94811f5bcc7c748eac4588c702e46e
Ryan Carless <ryancarless96@gmail.com>
2023-03-13 19:48:32 -0400

    changed App and utils folder

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,21 +2,21 @@ import { gql } from '@apollo/client';
 
 export const QUERY_TECH = gql`
   query tech {
-    tech {
+    product {
       _id
       name
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
+export const QUERY_MATCHES = gql`
+  query matches($_id: String) {
+    matches(_id: $_id) {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      product1
+      product2
+      product1_votes
+      product2_votes
     }
   }
 `;
